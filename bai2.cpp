8cf4905c67706e311a343baf07a154f9b84f6cae
thang08082003 <nvthang8803@gmail.com>
2024-10-28 15:41:21 +0700

    baitapbuoi1

--- bai2.cpp
+++ bai2.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 
 int main() {
-    int a = 254;
-    int b = 343;
+    int a = 6;
+    int b = 2;
     cout << "a + b = " <<a+b<< endl;
-   
+    cout << "a - b = " <<a-b<< endl;
+    cout << "a * b = " <<a*b<< endl;
+    cout << "a / b = " <<a/b<< endl;
     return 0;
 }
