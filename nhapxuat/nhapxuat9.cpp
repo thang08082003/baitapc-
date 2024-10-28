2faa6e9ec5575ac4df2f835bfab4c1ad33d62338
thang08082003 <nvthang8803@gmail.com>
2024-10-28 16:51:14 +0700

    nhapxuat

---
+++ nhapxuat/nhapxuat9.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char input;
+    
+    cin >> input;
+    char output = input + 1;
+    
+   
+    cout << output << endl;
+    
+    return 0;
+}
